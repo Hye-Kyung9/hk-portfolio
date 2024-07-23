444b0365d62a766a11e0b662c8c0e59c49ce3d8d
Hye-Kyung9 <rngprud123@naver.com>
2024-07-23 11:28:22 +0900

    feat: emotion설치 / globalStyle적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-function App() {
-  return (
-    <div className="App">
-      hello~
-      <br/>
-      hyekyung's portfolio
+import globalStyles from "./style/globalStyles";
+import { Global } from '@emotion/react';
+
+const App = () => (
+  <>
+    <Global styles={globalStyles} />
+    <div>
+      <h1>안녕하세요! abcd</h1>
+      <p>나눔바른고딕 폰트 abcdefg</p>
     </div>
-  );
-}
+  </>
+);
 
 export default App;
---
+++ src/style/globalStyles.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+const globalStyles = css`
+  @import url('https://hangeul.pstatic.net/hangeul_static/css/nanum-barun-gothic.css');
+
+  body {
+    font-family: 'Nanum Barun Gothic', sans-serif;
+  }
+`;
+
+export default globalStyles;
