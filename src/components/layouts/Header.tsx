409714c7c77e7ff4d8d84d1dc9a6d84ed787462a
Hye-Kyung9 <rngprud123@naver.com>
2024-07-25 13:40:30 +0900

    feat: lint수정 / logo 추가

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,3 +1,10 @@
+import logo from '../../assets/images/common/logo-blue.svg';
+
 export default function Header() {
-  return <div>헤더영역입니다</div>;
+  return (
+    <div style={{ display: 'flex' }}>
+      <img src={logo} style={{ width: '200px' }} alt="logo" />
+      헤더영역입니다
+    </div>
+  );
 }
